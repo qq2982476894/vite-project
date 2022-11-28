3014b28fc8e33b2d3e0aa2c2826414ce6f118350
wangsilong <984889623@qq.com>
2022-11-28 17:03:10 +0800

    布局和echarts

---
+++ src/components/echarts/bar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="chart_box" ref="chart_bar"></div>
+</template>
+
+<script setup>
+import * as echarts from "echarts"
+const props = defineProps(['chartData'])
+console.log(props.chartData,'chartData');
+const chart_bar = ref(null)
+const option = reactive({
+        xAxis: {
+          type: "category",
+          
+          data: [
+            "一月",
+            "二月",
+            "三月",
+            "四月",
+           
+          ]
+        },
+        tooltip: {
+          trigger: "axis"
+        },
+        yAxis: {
+          type: "value",
+          splitLine: {
+            show: false
+            },
+        },
+        grid:{
+            width:'80%',
+            left:'45'
+        },
+        series: [
+          {
+            data: [
+              820,
+              801,
+              102,
+              230,
+            ],
+            type: "bar",
+            barWidth:10,
+            smooth: true
+          }
+        ]
+      })
+onMounted(() => {
+    initChart()
+})
+
+// 基础配置一下Echarts
+function initChart() {
+      let chart = echarts.init(chart_bar.value);
+      // 把配置和数据放这里
+      option.xAxis.data = props.chartData.x;
+      option.series[0].data = props.chartData.y;
+
+      chart.setOption(option);
+      window.onresize = function() {
+        chart.resize();
+      };
+    }
+
+</script>
+
+<style lang="less" scoped>
+.chart_box{
+    height: 100%;
+}
+</style>
--- src/views/bigScreen/bigScreen.vue
+++ src/views/bigScreen/bigScreen.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="page">
-    <header>天气预报</header>
-    <div class="container">
-        <section class="left-box s-box"></section>
-        <section class="mid-box s-box"></section>
-        <section class="right-box s-box"></section>
+    <header class="header">天气预报</header>
+    <div class="container flex_row">
+      <section class="left-box flex_column">
+        <div class="base_card">
+          <div class="title">标题</div>
+          <div class="chart-box">
+            <Bar :chartData="chart1" />
+          </div>
+        </div>
+        <div class="base_card">
+          <div class="title">标题</div>
+          <div class="chart-box">
+            <Bar :chartData="chart2" />
+          </div>
+        </div>
+        <div class="base_card">
+          <div class="title">标题</div>
+          <div class="chart-box">
+            <Bar :chartData="chart3" />
+          </div>
+        </div>
+      </section>
+      <section class="mid-box flex_column">
+        <div class="flex_row">
+            <div class="base_card">
+          <div class="title">标题</div>
+          <div class="chart">图表</div>
+        </div>
+        </div>
+        <div class="flex_row">
+            <div class="base_card">
+          <div class="title">标题</div>
+          <div class="chart">图表</div>
+        </div>
+        </div>
+      </section>
+      <section class="right-box flex_column">
+        <div class="base_card">
+            <div class="title">标题</div>
+            <div class="chart">图表</div>
+          </div>
+          <div class="base_card">
+            <div class="title">标题</div>
+            <div class="chart">图表</div>
+          </div>
+      </section>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import Bar from '@/components/echarts/bar.vue'
+const chart1 = reactive({
+    x:['星期一','星期二','星期三','星期四','星期五']
+    ,y:['5','11','22','33']
+})
 </script>
 
 <style lang="less" src="./index.less">
